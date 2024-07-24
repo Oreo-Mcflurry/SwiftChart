ba3a3bad5ba95cbf324fc2602e3524e2f6235196
Musk <inhoyoo1029@gmail.com>
2024-07-24 16:32:09 +0900

    Initial Commit

---
+++ SwiftChart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftChart
+//
+//  Created by 에스지랩 on 7/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftChart/SwiftChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartApp.swift
+//  SwiftChart
+//
+//  Created by 에스지랩 on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
